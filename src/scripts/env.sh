7845736954c91190fd0df057fbc2f8c149c6976d
heyli <lcxfs1991@gmail.com>
2024-01-22 22:23:55 +0800

    feat: improve

--- src/scripts/env.sh
+++ src/scripts/env.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
+# #!/bin/bash
+# set -e # Exit with nonzero exit code if anything fails
 
-export GITHUB_TOKEN=$GITHUB_TOKEN
+# export GITHUB_TOKEN=$GITHUB_TOKEN
 
